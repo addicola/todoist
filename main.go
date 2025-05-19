abf7ac9fd4796764dfd484ec5905a1b656e19bf0
addicola <blue_lifesky@hotmail.com>
2025-05-20 00:41:41 +0700

    Todo create handler

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"github.com/addicola/todoist/todo"
+)
 
 func main() {
+	// Init database connection
+	db, err := gorm.Open(sqlite.Open("todo.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&todo.Todo{})
+
+	// Initialize the Todo handler
+	todoHandler := todo.NewTodoHandler(db)
+
 	r := gin.Default()
 
+	r.POST("/todos", todoHandler.CreateTodo)
+	
 	r.Run(":8080")
 }
--- todo/todo.go
+++ todo/todo.go
@@ -1,6 +1,9 @@
 package todo
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -11,4 +14,23 @@ type Todo struct {
 
 type TodoHandler struct {
 	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) CreateTodo(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := t.db.Create(&todo).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create todo"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, todo)
 }
