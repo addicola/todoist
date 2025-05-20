d0500e14ead185d01b0a01a88331e9ecd516e257
addicola <blue_lifesky@hotmail.com>
2025-05-20 22:57:29 +0700

    Add update todo by id

--- todo/todo.go
+++ todo/todo.go
@@ -8,6 +8,8 @@ import (
 	"gorm.io/gorm"
 )
 
+var validate = validator.New()
+
 type Todo struct {
 	ID          uint   `json:"id" gorm:"primaryKey"`
 	Title       string `json:"title" gorm:"not null" validate:"required"`
@@ -51,4 +53,30 @@ func (t *TodoHandler) CreateTodo(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, todo)
+}
+
+func (t *TodoHandler) UpdateTodo(c *gin.Context) {
+	var todo Todo
+	if err := t.db.First(&todo, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Todo not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Validator to ensure Title is not empty
+	if err := validate.Struct(todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := t.db.Save(&todo).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update todo"})
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
 }
